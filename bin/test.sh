d575661c177ace8a732b061922957d3a203aff02
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-07-23 23:21:21 +0900

    ./bin/test.sh の成功時の表示を改善

--- bin/test.sh
+++ bin/test.sh
@@ -16,3 +16,13 @@ python -m pycodestyle "${SRC_DIR}/janken_cli_application.py"
 PYTHONPATH="${SRC_DIR}" python -m pytest
 
 echo -e "0\n0" | python "${SRC_DIR}/janken_cli_application.py"
+
+set +o xtrace
+GREEN='\e[0;32m'
+WRITE='\e[0;37m'
+echo -en "${GREEN}"
+echo '=============='
+echo 'TEST PASSED!!!'
+echo '=============='
+echo -en "${WRITE}"
+set -o xtrace
