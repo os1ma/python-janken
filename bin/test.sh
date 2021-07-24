952037d023e70c257a4af8388e94196e8fa51bda
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-07-24 20:41:48 +0900

    ./bin/test.sh に mypy の解析を追加

--- bin/test.sh
+++ bin/test.sh
@@ -11,12 +11,19 @@ readonly SRC_DIR="${PROJECT_HOME}/janken"
 
 cd "${PROJECT_HOME}"
 
+# Static Checking
 python -m pycodestyle "${SRC_DIR}"
+cd "${SRC_DIR}"
+python -m mypy --explicit-package-bases --namespace-packages .
+cd -
 
+# Unit Testing
 PYTHONPATH="${SRC_DIR}" python -m pytest
 
+# Integration Testing
 echo -e "0\n0" | python "${SRC_DIR}/janken_cli_application.py"
 
+# Result
 set +o xtrace
 GREEN='\e[0;32m'
 WRITE='\e[0;37m'
